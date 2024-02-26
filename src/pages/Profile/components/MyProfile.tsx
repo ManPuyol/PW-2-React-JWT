4e6fcfe81597bb8c4c78a877a9e29a0c15371d24
Manuel Ortega <maopmore@gmail.com>
2024-02-26 20:11:17 +0100

    Vista adaptada a la entrega 1

--- src/pages/Profile/components/MyProfile.tsx
+++ src/pages/Profile/components/MyProfile.tsx
@@ -21,6 +21,8 @@ import CountrySelector from './CountrySelector';
 import EditorToolbar from './EditorToolbar';
 
 export default function MyProfile() {
+
+
   return (
     <Box
       sx={{
@@ -29,103 +31,7 @@ export default function MyProfile() {
         mx: 'auto',
       }}
     >
-      <Tabs defaultValue={0} sx={{ bgcolor: 'transparent' }}>
-        <Box
-          sx={{
-            '--_shadow-height': '16px',
-            height: 0,
-            position: 'sticky',
-            top: 'calc(48px - var(--main-paddingTop, 0px) + var(--Header-height, 0px) - (var(--_shadow-height) / 2))',
-            zIndex: 1,
-            '&::before': {
-              content: '""',
-              display: 'block',
-              position: 'relative',
-              zIndex: 1,
-              height: 'var(--_shadow-height)',
-              background:
-                'radial-gradient(closest-side, rgba(0 0 0 / 0.12), transparent 100%)',
-            },
-          }}
-        />
-        <TabList
-          sticky="top"
-          variant="plain"
-          sx={(theme) => ({
-            '--Chip-minHeight': '20px',
-            '--ListItem-minHeight': '48px',
-            top: 'calc(-1 * (var(--main-paddingTop, 0px) - var(--Header-height, 0px)))',
-            zIndex: 10,
-            width: '100%',
-            overflow: 'auto hidden',
-            alignSelf: 'flex-start',
-            scrollSnapType: 'inline',
-            '&::after': {
-              pointerEvents: 'none',
-              display: { xs: 'block', sm: 'none' },
-              content: '""',
-              position: 'sticky',
-              top: 0,
-              width: 40,
-              flex: 'none',
-              zIndex: 1,
-              right: 0,
-              borderBottom: '1px solid transparent',
-              background: `linear-gradient(to left, ${theme.vars.palette.background.body}, rgb(0 0 0 / 0))`,
-              backgroundClip: 'content-box',
-            },
-            '&::-webkit-scrollbar': {
-              width: 0,
-              display: 'none',
-            },
-            [`& .${tabClasses.root}`]: {
-              '--focus-outline-offset': '-2px',
-              '&:first-of-type': {
-                ml: 'calc(-1 * var(--ListItem-paddingX))',
-              },
-              scrollSnapAlign: 'start',
-              bgcolor: 'transparent',
-              flex: 'none',
-              '&:hover': {
-                bgcolor: 'transparent',
-              },
-              [`&.${tabClasses.selected}`]: {
-                color: 'primary.plainColor',
-                bgcolor: 'transparent',
-                [`& .${chipClasses.root}`]: theme.variants.solid.primary,
-              },
-            },
-          })}
-        >
-          <Tab indicatorInset value={0}>
-            Account settings
-          </Tab>
-          <Tab indicatorInset value={1}>
-            Team{' '}
-            <Chip size="sm" variant="soft" color="neutral" sx={{ ml: 1 }}>
-              2
-            </Chip>
-          </Tab>
-          <Tab indicatorInset value={2}>
-            Plan
-          </Tab>
-          <Tab indicatorInset value={3}>
-            Billing{' '}
-            <Chip size="sm" variant="soft" color="neutral" sx={{ ml: 1 }}>
-              4
-            </Chip>
-          </Tab>
-          <Tab indicatorInset value={4}>
-            Notifications
-          </Tab>
-          <Tab indicatorInset value={5}>
-            Integrations
-          </Tab>
-          <Tab indicatorInset value={6}>
-            API
-          </Tab>
-        </TabList>
-        <Box
+      <Box
           sx={{
             pt: 3,
             pb: 10,
@@ -143,16 +49,11 @@ export default function MyProfile() {
           }}
         >
           <FormLabel sx={{ display: { xs: 'none', sm: 'block' } }}>
-            Name
+            Username
           </FormLabel>
-          <Box sx={{ display: { xs: 'contents', sm: 'flex' }, gap: 2 }}>
+          <Box sx={{ display: { sm: 'contents' }}}>
             <FormControl sx={{ flex: 1 }}>
-              <FormLabel sx={{ display: { sm: 'none' } }}>First name</FormLabel>
-              <Input placeholder="first name" defaultValue="Siriwat" />
-            </FormControl>
-            <FormControl sx={{ flex: 1 }}>
-              <FormLabel sx={{ display: { sm: 'none' } }}>Last name</FormLabel>
-              <Input placeholder="last name" defaultValue="K." />
+              <Input placeholder="username" defaultValue="Siriwat" />
             </FormControl>
           </Box>
           <Divider role="presentation" />
@@ -160,103 +61,36 @@ export default function MyProfile() {
             <FormLabel>Email</FormLabel>
             <Input
               type="email"
-              startDecorator={<i data-feather="mail" />}
+              //startDecorator={<i data-feather="mail" />}
               placeholder="email"
               defaultValue="siriwatk@test.com"
             />
           </FormControl>
-          <Divider role="presentation" />
-          <div>
-            <FormLabel>Your photo</FormLabel>
-            <FormHelperText>
-              This will be displayed on your profile.
-            </FormHelperText>
-          </div>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              flexWrap: 'wrap',
-              gap: 2.5,
-            }}
-          >
-            <Avatar
-              size="lg"
-              src="/static/images/avatar/1.jpg"
-              sx={{ '--Avatar-size': '64px' }}
-            />
-            <DropZone />
-          </Box>
+          
+
           <Divider role="presentation" />
           <FormControl sx={{ display: { sm: 'contents' } }}>
             <FormLabel>Role</FormLabel>
             <Input defaultValue="UI Developer" />
           </FormControl>
           <Divider role="presentation" />
-          <CountrySelector />
-          <Divider role="presentation" />
-          <FormControl sx={{ display: { sm: 'contents' } }}>
-            <FormLabel>Timezone</FormLabel>
-            <Select
-              startDecorator={<i data-feather="clock" />}
-              defaultValue="1"
-            >
-              <Option value="1">
-                Indochina Time (Bangkok){' '}
-                <Typography textColor="text.tertiary" ml={0.5}>
-                  — GMT+07:00
-                </Typography>
-              </Option>
-              <Option value="2">
-                Indochina Time (Ho Chi Minh City){' '}
-                <Typography textColor="text.tertiary" ml={0.5}>
-                  — GMT+07:00
-                </Typography>
-              </Option>
-            </Select>
-          </FormControl>
-          <Divider role="presentation" />
+          {/* <CountrySelector />
+          <Divider role="presentation" /> */}
           <div>
-            <FormLabel>Bio</FormLabel>
-            <FormHelperText>Write a short introduction.</FormHelperText>
+            <FormLabel>Change password</FormLabel>
           </div>
           <div>
-            <EditorToolbar />
-            <Textarea
-              minRows={4}
-              sx={{ mt: 1.5 }}
-              defaultValue="I'm a software developer based in Bangkok, Thailand. My goal is to solve UI problems with neat CSS without using too much JavaScript."
-            />
-            <FormHelperText sx={{ mt: 0.75, fontSize: 'xs' }}>
-              275 characters left
-            </FormHelperText>
+            <FormControl sx={{ flex: 1 , marginBottom: 2 }}>
+              <FormLabel >New password</FormLabel>
+              <Input type='password' placeholder="first name" />
+            </FormControl>
+            <FormControl sx={{ flex: 1 }}>
+              <FormLabel >Confirm password</FormLabel>
+              <Input type='password' placeholder="last name"  />
+            </FormControl>
           </div>
           <Divider role="presentation" />
-          <div>
-            <FormLabel>Portfolio projects</FormLabel>
-            <FormHelperText>Share a few snippets of your work.</FormHelperText>
-          </div>
-          <Stack useFlexGap spacing={1.5}>
-            <DropZone />
-            <FileUpload
-              fileName="Tech design requirements.pdf"
-              fileSize="200 KB"
-              progress={100}
-            />
-            <FileUpload
-              icon={<i data-feather="film" />}
-              fileName="Dashboard prototype recording.mp4"
-              fileSize="16 MB"
-              progress={40}
-            />
-            <FileUpload
-              icon={<i data-feather="upload-cloud" />}
-              fileName="Dashboard prototype FINAL.fig"
-              fileSize="4.2 MB"
-              progress={80}
-            />
-          </Stack>
-          <Divider role="presentation" />
+
           <Box
             sx={{
               gridColumn: '1/-1',
@@ -271,7 +105,6 @@ export default function MyProfile() {
             <Button size="sm">Save</Button>
           </Box>
         </Box>
-      </Tabs>
     </Box>
   );
 }
