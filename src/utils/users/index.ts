a9e2d02c3df3f18be0110bb16889525821fe7fca
Manuel Ortega <maopmore@gmail.com>
2024-03-01 21:36:55 +0100

    temporary user list

---
+++ src/utils/users/index.ts
@@ -0,0 +1,135 @@
+export function getUsers() {
+    return [
+        {
+          "id": 1,
+          "username": "manuel",
+          "email": "manuel@gmail.com",
+          "password": "$2a$08$W2Wp4IleaJsyYVbZhaScnOaeqcj2i8axoFWxt0VnPdngCKsMfgczu",
+          "createdAt": "2023-12-03T20:28:49.000Z",
+          "updatedAt": "2023-12-06T14:04:14.000Z",
+          "roles": [
+            "user",
+            "moderator",
+            "admin"
+          ]
+        },
+        {
+          "id": 2,
+          "username": "John",
+          "email": "john@email.com",
+          "password": "$2a$08$JBjeeerB4un53TAR97lgh.vjbFH0N8dizKZxH67f/BRGnCE86mWcS",
+          "createdAt": "2023-12-06T11:30:08.000Z",
+          "updatedAt": "2023-12-06T11:30:08.000Z",
+          "roles": [
+            "user",
+            "moderator"
+          ]
+        },
+        {
+          "id": 3,
+          "username": "Jane",
+          "email": "jane@email.com",
+          "password": "$2a$08$kvSs/RxIfGukcJUTFHGNAu0x0uA5GjvxecXDD5GMLOSxM.9x32qoy",
+          "createdAt": "2023-12-06T11:32:52.000Z",
+          "updatedAt": "2023-12-06T11:32:52.000Z",
+          "roles": [
+            "user"
+          ]
+        },
+        {
+          "id": 4,
+          "username": "Andres",
+          "email": "andypin2002@mi.mama",
+          "password": "$2a$08$9zy5v/HTJeCPubK0w.REaOtEyBA6bYKexo4prbvpLMkKB/qu/7Vjq",
+          "createdAt": "2023-12-06T14:10:19.000Z",
+          "updatedAt": "2023-12-06T14:11:00.000Z",
+          "roles": [
+            "user"
+          ]
+        },
+        {
+          "id": 5,
+          "username": "Jose",
+          "email": "a@a.a",
+          "password": "$2a$08$IeHT.KAz7TjY6c77fGp2yuDQcVp6iqph8/RYWuqt1WMcpYaVHe5Yi",
+          "createdAt": "2023-12-07T08:32:41.000Z",
+          "updatedAt": "2023-12-07T08:32:41.000Z",
+          "roles": [
+            "user"
+          ]
+        },
+        {
+          "id": 6,
+          "username": "julio Iglesias",
+          "email": "julio.iglesias@gmail.com",
+          "password": "$2a$08$qYljuhd/o0CAVKcuHptrHOml2d6NEmvs4hc3S9SdJSrG.5mYDGvSm",
+          "createdAt": "2023-12-07T08:39:30.000Z",
+          "updatedAt": "2023-12-07T08:39:30.000Z",
+          "roles": [
+            "user"
+          ]
+        },
+        {
+          "id": 7,
+          "username": "alejandro",
+          "email": "aledevme@gmail.com",
+          "password": "$2a$08$SkGHmPPWydzty9ksFya2S.tJP26/hUoZg1ZMMwc00TDNHgWcorXb2",
+          "createdAt": "2023-12-07T08:42:51.000Z",
+          "updatedAt": "2023-12-07T08:42:51.000Z",
+          "roles": [
+            "user",
+            "moderator"
+          ]
+        },
+        {
+          "id": 8,
+          "username": "David",
+          "email": "david@email.com",
+          "password": "$2a$08$sKUgVfNHA4wKyg6l2MsAQeHbTpl/XnV5r5oDvb/LnjjnAB72vLdMe",
+          "createdAt": "2023-12-07T17:43:39.000Z",
+          "updatedAt": "2023-12-07T17:43:40.000Z",
+          "roles": [
+            "user",
+            "moderator"
+          ]
+        },
+        {
+          "id": 9,
+          "username": "werwer",
+          "email": "we@rw.er",
+          "password": "$2a$08$3gZ4gzhVf5FkcO9QqA.Pt.BhUOHC7vXJ6a0Qpq7IlvbsI/kJi1cN2",
+          "createdAt": "2024-02-26T17:58:30.000Z",
+          "updatedAt": "2024-02-26T17:58:30.000Z",
+          "roles": [
+            "user"
+          ]
+        },
+        {
+          "id": 10,
+          "username": "NOmanuel",
+          "email": "a@a.aa",
+          "password": "$2a$08$mWIarFq8Rkmn03zDoH0RD.SjbWErZUpA9Im60wwdqVKSP5T5fuIwy",
+          "createdAt": "2024-02-26T17:59:35.000Z",
+          "updatedAt": "2024-02-26T17:59:35.000Z",
+          "roles": [
+            "user"
+          ]
+        },
+        {
+          "id": 11,
+          "username": "Juan",
+          "email": "Juan@email.com",
+          "password": "$2a$08$..xhY1V6seyzNLIcr2yj3OZlvJXVvwTxFy60R6V3/PB6sERZeDK5m",
+          "createdAt": "2024-02-26T18:29:40.000Z",
+          "updatedAt": "2024-02-26T18:29:40.000Z",
+          "roles": [
+            "user"
+          ]
+        }
+      ]
+
+    // fetch(`${import.meta.env.VITE_API_URL}/users`, {
+    //     method: "GET",
+    // }).then(function (res) { return res.json(); })
+
+}
